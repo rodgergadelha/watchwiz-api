ed8766a7a65feada0294e1a254493294eb38308f
rodgergadelha <iamapicle64@gmail.com>
2023-06-23 23:38:33 -0300

    update

--- src/main/java/com/example/movieappbackend/api/controller/UserController.java
+++ src/main/java/com/example/movieappbackend/api/controller/UserController.java
@@ -151,7 +151,7 @@ public class UserController {
 		return ResponseEntity.ok("Email updated successfully!");
 	}
 	
-	@GetMapping(value = "/my-account/profile-image", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@GetMapping(value = "/my-account/profile-image", produces = MediaType.IMAGE_JPEG_VALUE)
 	public ResponseEntity<Resource> profileImage() {
 		Resource resource = service.getProfileImage();
 		return resource == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(resource);
